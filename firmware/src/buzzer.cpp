e80933ec3bbe61ed418c71cab593b0097507e867
Christophe Clienti <cclienti@users.noreply.github.com>
2021-05-13 10:05:24 +0200

    Added the GPL-V3 license header

--- firmware/src/buzzer.cpp
+++ firmware/src/buzzer.cpp
@@ -1,3 +1,19 @@
+// This file is part of the tig-welder project
+// (https://github.com/cclienti/tig-welder)
+// Copyright (c) 2021 Christophe Clienti
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 #include "tig-welder/buzzer.h"
 
 #include "hardware/pwm.h"
